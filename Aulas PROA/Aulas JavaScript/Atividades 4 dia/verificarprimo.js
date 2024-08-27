58d6d367d7cf97636c8df57173437e12c322681b
vic-fmr <vfm@poli.br>
2024-08-26 21:33:26 -0300

    ajustes

--- Aulas PROA/Aulas JavaScript/Atividades 4 dia/verificarprimo.js
+++ Aulas PROA/Aulas JavaScript/Atividades 4 dia/verificarprimo.js
@@ -1,14 +1,11 @@
 function verificarprimo(){
     let numero = parseFloat(document.getElementById('primo').value);
 
-    if(numero >= 4){
-        if(numero%2 == 0 || numero%3 == 0 ){
+    if(numero >= 4 && (numero%2 == 0 || numero%3 == 0)){
         document.getElementById('resultadoprimo').innerText = "Não é primo!";
     } else{
         document.getElementById('resultadoprimo').innerText = "É primo!";
-    }
-} else{
-    document.getElementById('resultadoprimo').innerText = "É primo!";
+    
 }
 document.getElementById('primo').innerText = "";
 }
