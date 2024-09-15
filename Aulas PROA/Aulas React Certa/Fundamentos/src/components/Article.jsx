881cf53d88adcf655e798f61053967c5791cb817
vic-fmr <vfm@poli.br>
2024-09-15 19:30:26 -0300

    Início REACT

---
+++ Aulas PROA/Aulas React Certa/Fundamentos/src/components/Article.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Article(){
+    return(
+        <article>
+            <h2>Main Article</h2>
+            <p>Conteudo do Artigo</p>
+        </article>
+    )
+}
+
+export default Article;
