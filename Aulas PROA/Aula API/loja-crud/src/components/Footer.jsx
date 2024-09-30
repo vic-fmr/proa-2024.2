f5812ad337f4989e066c3d52546b25f9c5ea6ac2
vic-fmr <vfm@poli.br>
2024-09-30 17:50:45 -0300

    Aula CRUD React

---
+++ Aulas PROA/Aula API/loja-crud/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return(
+        <footer className="footer">
+            <p>&copy; 2024 FashionStore LTDA - Todos os direitos reservados.</p>
+        </footer>
+    );
+}
+
+export default Footer;
